bee9391bfc783a6223cd3234d1e6a023f0e5b781
Ankit Prasad <ankitbadoni.work@gmail.com>
2025-06-10 17:20:20 +0530

    change in app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from src.logic import greet_user
 
-st.title("Streamlit CI/CD Example")
+st.title("Streamlit CI/CD Demo")
 name = st.text_input("Enter your name")
 
 if name:
